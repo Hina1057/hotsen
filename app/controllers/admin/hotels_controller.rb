2afcd06349c9588d7ce45f42b7c99734b2ffcde2
Hinata Imaizumi <ne241057@senshu-u.jp>
2026-01-07 14:24:05 +0900

    ホテルの追加編集

--- app/controllers/admin/hotels_controller.rb
+++ app/controllers/admin/hotels_controller.rb
@@ -1,12 +1,51 @@
 class Admin::HotelsController < Admin::ApplicationController
   before_action :require_admin_login
+  before_action :set_hotel, only: [:show, :edit, :update]
 
   def index
     @hotels = Hotel.order(:id)
   end
 
   def show
-    @hotel = Hotel.find(params[:id])
     @rooms = @hotel.rooms.order(:id)
+  end
+
+  def new
+    @hotel = Hotel.new
+  end
+
+  def create
+    @hotel = Hotel.new(hotel_params)
+    if @hotel.save
+      redirect_to admin_hotels_path, notice: "ホテルを追加しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @hotel.update(hotel_params)
+      redirect_to admin_hotel_path(@hotel), notice: "ホテル情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_hotel
+    @hotel = Hotel.find(params[:id])
+  end
+
+  def hotel_params
+    params.require(:hotel).permit(
+      :name, :address, :area, :phone_number,
+      :wifi, :large_bath, :openair_bath, :sauna, :bedrock_bath,
+      :barrier_free, :smoking_area,
+      :information, :hotel_photo
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,8 +54,7 @@ Rails.application.routes.draw do
     resources :admins, only: [:index, :new, :create, :edit, :update, :destroy]
 
     # ホテル管理（一覧・詳細）
-    resources :hotels, only: [:index, :show, :new, :create] do
-      
+    resources :hotels, only: [:index, :show, :new, :create, :edit, :update] do
       resources :rooms, only: [:new, :create, :edit, :update, :destroy]
     end
 
